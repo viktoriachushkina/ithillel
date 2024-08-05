f6e5711434541249132966e24e9557b819fbe87f
Chushkina Viktoria <viktoriacuskina@gmail.com>
2024-08-05 22:22:26 +0300

    Add tasks with my homework №4

---
+++ jsBasics/task2.js
@@ -0,0 +1,11 @@
+// task2.js
+const person1 = "Viktoria";
+const person2 = "Ross";
+
+// Конкатенація рядків
+const greetingConcat = "Hello, " + person1 + " and " + person2 + "!";
+console.log("Greeting using concatenation:", greetingConcat);
+
+// Шаблонний рядок
+const greetingTemplate = `Hello, ${person1} and ${person2}!`;
+console.log("Greeting using template string:", greetingTemplate);
---
+++ jsBasics/task3.js
@@ -0,0 +1,7 @@
+// task3.js
+const ageT = 21;
+const ageF = 11;
+const isAdult = ageT >= 18;
+
+console.log("Age:", ageT);
+console.log("Is adult:", isAdult);
