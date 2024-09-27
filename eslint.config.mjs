a1879ab526bba95982aed837d2aca02b2dbb6e6e
Chushkina Viktoria <viktoriacuskina@gmail.com>
2024-09-27 21:03:32 +0300

    Test ESLint pre-commit hook

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
+{
+  "extends": [
+    "eslint:recommended",
+    "plugin:prettier/recommended"
+  ],
+  "rules": {
+    "prettier/prettier": "error"
+  }
+}
--- homework11/task3.js
+++ homework11/task3.js
@@ -42,3 +42,10 @@ async function executeRequests() {
 }
 
 executeRequests();
+console.log("Начинаем готовить");
+
+setTimeout(() => {
+  console.log("Вода закипела");
+}, 3000); // Через 3 секунды
+
+console.log("Режем овощи");
